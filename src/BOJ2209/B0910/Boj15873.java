38a65d67f3c6a590f9dc4d9f4b7b9994cec478f8
Woongbin06 <kob05314@gmail.com>
2022-09-10 23:28:54 +0900

    20220908 commit

---
+++ src/BOJ2209/B0910/Boj15873.java
@@ -0,0 +1,16 @@
+package BOJ2209.B0910;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj15873 {
+    public static void main(String[] args) throws IOException {
+        String x;
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        x = bf.readLine();
+
+        int n = x.charAt(0) - '0';
+    }
+}
