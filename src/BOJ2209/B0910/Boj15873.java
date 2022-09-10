b133db212d375d09c7c554c88743c10b9136ad4b
Woongbin06 <kob05314@gmail.com>
2022-09-10 23:31:48 +0900

    20220908 commit

--- src/BOJ2209/B0910/Boj15873.java
+++ src/BOJ2209/B0910/Boj15873.java
@@ -12,5 +12,12 @@ public class Boj15873 {
         x = bf.readLine();
 
         int n = x.charAt(0) - '0';
+        if(x.charAt(1) == '0')
+            n = n * 10 + (x.charAt(1) - '0');
+        int m = x.charAt(2) - '0';
+        if(x.charAt(3) == '0')
+            m = m * 10 + (x.charAt(3) - '0');
+
+        System.out.println(n + m);
     }
 }
